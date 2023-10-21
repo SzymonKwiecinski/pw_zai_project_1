0dd47daefcd52a0fe3beecaf9aab2f1140a34711
kwiecs01 <kwiecinskji@gmail>
2023-10-21 19:20:20 +0200

    SQL DONE

--- queries/create_table.sql
+++ queries/create_table.sql
@@ -1,23 +1,23 @@
 CREATE TABLE users
 (
     id       SMALLSERIAL PRIMARY KEY,
-    nick     VARCHAR(64)  NOT NULL,
-    email    VARCHAR(64)  NOT NULL,
-    password VARCHAR(128) NOT NULL
+    nick     VARCHAR(64) UNIQUE  NOT NULL,
+    email    VARCHAR(64) UNIQUE  NOT NULL,
+    password VARCHAR(128) UNIQUE NOT NULL
 );
 
 CREATE TABLE icons
 (
     id    SMALLSERIAL PRIMARY KEY,
-    name  VARCHAR(64) NOT NULL,
-    value TEXT        NOT NULL
+    name  VARCHAR(64) UNIQUE NOT NULL,
+    value TEXT               NOT NULL
 );
 
 CREATE TABLE categories
 (
     id      SMALLSERIAL PRIMARY KEY,
-    name    VARCHAR(32) NOT NULL,
-    color   CHAR(7),
+    name    VARCHAR(32) UNIQUE NOT NULL,
+    color   VARCHAR(7),
     icon_id SMALLINT REFERENCES icons (id) ON DELETE CASCADE,
     CONSTRAINT fk_icon FOREIGN KEY (icon_id) REFERENCES icons (id)
 );
@@ -30,8 +30,9 @@ CREATE TABLE events
     graphic     VARCHAR(64),
     start_date  DATE        NOT NULL,
     end_date    DATE        NOT NULL,
-    user_id     SMALLINT REFERENCES users (id) ON DELETE SET NULL,
+    user_id     SMALLINT    REFERENCES users (id) ON DELETE SET NULL,
     category_id SMALLINT REFERENCES categories (id) ON DELETE RESTRICT,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id)
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id),
+    CHECK ( start_date <= end_date )
 );
