e41a7a425709a0d676f6a1a59adaca7e4ec5c171
kwiecs01 <kwiecinskji@gmail>
2023-10-21 19:29:23 +0200

    modeles

--- timeline_app/models.py
+++ timeline_app/models.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import List
+from typing import List, Optional
 
 from sqlmodel import create_engine, Field, SQLModel, Session, select, Relationship, ForeignKey
 # https://github.com/tiangolo/sqlmodel/issues/213
@@ -9,50 +9,42 @@ engine = create_engine(url, echo=True)
 
 
 class Users(SQLModel, table=True):
-    id: int = Field( primary_key=True, ge=-32768, le=32767)
-    name: str = Field(max_length=64)
+    id: int = Field(primary_key=True, ge=-32768, le=32767)
+    nick: str = Field(max_length=64)
     email: str = Field(max_length=64)
     password: str = Field(max_length=128)
-    events: List["Events"] = Relationship(sa_relationship_kwargs={"cascade": "all, delete"})
+    # events: List["Events"] = Relationship(sa_relationship_kwargs={"cascade": "all, delete"})
 
 
 class Icons(SQLModel, table=True):
     id: int = Field(primary_key=True, ge=-32768, le=32767)
     email: str = Field(max_length=64)
-    password: str
+    value: str
 
 
 class Categories(SQLModel, table=True):
     id: int = Field(primary_key=True, ge=-32768, le=32767)
     name: str = Field(max_length=64)
-    color: str = Field(max_length=7)
+    color: Optional[str] = Field(None, max_length=7)
     icon_id: int = Field(foreign_key="icons.id", ge=-32768, le=32767)
 
 
 class Events(SQLModel, table=True):
     id: int = Field(primary_key=True, ge=-2147483648, le=2147483647)
-    name: str = Field(max_length=128)
-    description: str
-    graphic: str = Field(max_length=64)
+    name: str = Field(max_length=64)
+    description: Optional[str] = Field(None)
+    graphic: Optional[str] = Field(None, max_length=64)
     start_date: datetime.date
     end_date: datetime.date
-    user_id: int = Field(foreign_key="users.id", ge=-32768, le=32767, ForeignKey("users.id", ondelete="CASCADE"))
-    category_id: int = Field(foreign_key="categories.id", ge=-32768, le=32767)
+    user_id: Optional[int] = Field(None, foreign_key="users.id", ge=-32768, le=32767)
+    category_id: Optional[int] = Field(None, foreign_key="categories.id", ge=-32768, le=32767)
 
 
-# def select_heroes():
-#     with Session(engine) as session:
-#         statement = select(test_table).where(test_table.name == 'szymon')
-#         res = session.exec(statement)
-#         for x in res:
-#             print(x)
+def select_heroes():
+    with Session(engine) as session:
+        statement = select(Users).where(Users.id == 'szymon')
+        res = session.exec(statement)
+        for x in res:
+            print(x)
 
 
-# select_heroes()
-def create_db_and_tables():
-    # SQLModel.metadata.drop_all()
-    SQLModel.metadata.create_all(engine)
-
-
-
-create_db_and_tables()
